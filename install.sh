c66b88bcbab298e7540a496d37d4b8cf14d3c6da
habbis <habbis@medmail.ch>
2019-08-20 07:18:57 +0200

    added install script for symlink dotfiles

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+command_exists() {
+    type "$1" > /dev/null 2>&1
+}
+
+echo "Installing dotfiles."
+
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+
+source install/link.sh
+
+#source install/git.sh
+
+
+#echo "creating vim directories"
+#mkdir -p ~/.vim-tmp
+
+echo "Done. Reload your terminal."
